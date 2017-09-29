4c2cec4d171eece162c0d2f1badca4d2b8b62512
91094999 <91094999@mvusd.net>
2017-09-29 13:33:35 -0700

    HiLow

--- src/org/teachingkidsprogramming/section03ifs/HiLow2.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow2.java
@@ -3,24 +3,28 @@ package org.teachingkidsprogramming.section03ifs;
 import org.teachingextensions.logo.Sound;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
-public class HiLow
+// this variation tells user how many guesses they have left
+public class HiLow2
 {
   public static void main(String[] args)
   {
-    //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
     //int i;
     int answer = (int) (Math.random() * 100 + 1);
     // System.out.println("The answer is " + answer);
     for (int i = 0; i <= 8; i++)
     {
-      int guess = MessageBox.askForNumericalInput("What is your guess?");
+      int guess = 4;
+      if (guess == 0)
+      {
+        MessageBox.showMessage("You have 4 guesses left");
+      }
+      guess = MessageBox.askForNumericalInput("What is your guess?");
       if (guess == answer)
       {
         Sound.playBeep();
         MessageBox.showMessage("You won!");
         break;
       }
-      //     Otherwise, if the guess is too high #6
       else if (guess > answer)
       {
         MessageBox.showMessage("Too High");
@@ -31,7 +35,7 @@ public class HiLow
       }
       if (i == 8)
       {
-        MessageBox.showMessage("You tried");
+        MessageBox.showMessage("Your bad kid");
       }
     }
   }
