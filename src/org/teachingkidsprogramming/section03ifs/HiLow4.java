d057292a39f843b7706a65da483e88bea0c25bc2
91094999 <91094999@mvusd.net>
2017-10-02 11:01:38 -0700

    HiLow and started ConcentricLoop

--- src/org/teachingkidsprogramming/section03ifs/HiLow4.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow4.java
@@ -3,23 +3,17 @@ package org.teachingkidsprogramming.section03ifs;
 import org.teachingextensions.logo.Sound;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
-public class HiLow3
+public class HiLow4
 {
   public static void main(String[] args)
   {
-    int upperRange = 100;
     //int i;
     int answer = (int) (Math.random() * 100 + 1);
-    System.out.println("The answer is " + answer);
-    for (int i = 1; i <= 8; i++)
+    // System.out.println("The answer is " + answer);
+    for (int i = 0; i <= 8; i++)
     {
-      int guess = MessageBox.askForNumericalInput("What is your guess?");
-      while (guess < 1 || guess > 100)
+      int guess = MessageBox.askForNumericalInput("Your guess?");
       //input validation
-      {
-        MessageBox.showMessage("Please enter a valid guess between 1 and " + upperRange);
-        guess = MessageBox.askForNumericalInput("What is your guess?");
-      }
       if (guess == answer)
       {
         Sound.playBeep();
