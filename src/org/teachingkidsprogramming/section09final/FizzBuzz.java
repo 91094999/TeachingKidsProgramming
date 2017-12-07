5b343be7303405fad464e998f500a9b0ace09121
91094999 <91094999@mvusd.net>
2017-12-07 14:30:43 -0800

    FizzBuzz

--- src/org/teachingkidsprogramming/section09final/FizzBuzz.java
+++ src/org/teachingkidsprogramming/section09final/FizzBuzz.java
@@ -2,9 +2,31 @@ package org.teachingkidsprogramming.section09final;
 
 public class FizzBuzz
 {
-  //   For the whole numbers from 1 to 100, print either that number, or, 
-  //    If that number is divisible by 3, then print the word 'Fizz', 
-  //    If that number is divisible by 5, then print the word 'Buzz', 
-  //    If that number is divisible by 3 and 5, then print the word 'FizzBuzz'
-  //
+  public static void main(String[] args)
+  {
+    //   For the whole numbers from 1 to 100,
+    for (int i = 0; i <= 100; i++)
+    {
+      //    If that number is divisible by 3 and 5, then print the word 'FizzBuzz'
+      if (i % 15 == 0)
+      {
+        System.out.println("Fizzbuzz");
+      }
+      //    If that number is divisible by 3, then print the word 'Fizz', 
+      else if (i % 3 == 0)
+      {
+        System.out.println("Fizz");
+      }
+      //    If that number is divisible by 5, then print the word 'Buzz', 
+      else if (i % 5 == 0)
+      {
+        System.out.print("Buzz");
+      }
+      else
+      {
+        System.out.println(i);
+        //print either that number, or, 
+      }
+    }
+  }
 }
--- src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
+++ src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
@@ -18,7 +18,35 @@ public class FizzBuzzTDD
   @Test
   public void test1Returns1()
   {
-    //String result = FizzBuzzTDD.convert(1);
-    assertEquals(1, 0);
+    String result = FizzBuzzTDD.convert(1);
+    assertEquals("1", result);
+  }
+  public static String convert(int num)
+  {
+    if (num % 15 == 0)
+      return "FizzBuzz";
+    if (num % 3 == 0)
+      return "Fizz";
+    if (num % 5 == 0)
+      return "Buzz";
+    return num + "";
+  }
+  @Test
+  public void test3ReturnsFizz()
+  {
+    String result = FizzBuzzTDD.convert(3);
+    assertEquals("Fizz", result);
+  }
+  @Test
+  public void test5ReturnsBuzz()
+  {
+    String result = FizzBuzzTDD.convert(5);
+    assertEquals("Buzz", result);
+  }
+  @Test
+  public void test15ReturnsFizzBuzz()
+  {
+    String result = FizzBuzzTDD.convert(15);
+    assertEquals("FizzBuzz", result);
   }
 }
