06487b57fb67ba54de615635e95e22d4fcec0b73
91094999 <91094999@mvusd.net>
2017-12-11 10:58:28 -0800

    Stuff

--- src/org/teachingkidsprogramming/section08events/ConnectTheDots.java
+++ src/org/teachingkidsprogramming/section08events/ConnectTheDots.java
@@ -6,6 +6,8 @@ import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 import org.teachingextensions.logo.utils.EventUtils.MouseLeftClickListener;
 import org.teachingextensions.logo.utils.EventUtils.MouseRightClickListener;
 
+import javafx.scene.shape.Circle;
+
 public class ConnectTheDots implements MouseRightClickListener, MouseLeftClickListener
 {
   public static void main(String[] args)
@@ -17,6 +19,7 @@ public class ConnectTheDots implements MouseRightClickListener, MouseLeftClickLi
     setUpTheWindow();
     prepareColorPalette();
     //    Listen for left clicks on the window for the tortoise  --#1
+    Tortoise.getBackgroundWindow().addMouseLeftClickListener(this);
     //    Listen for right clicks on the window for the tortoise  --#20.2
   }
   @Override
@@ -25,16 +28,23 @@ public class ConnectTheDots implements MouseRightClickListener, MouseLeftClickLi
     //    addDot at x and y (recipe below) --#5
     //    ------------- Recipe for addDot --#6
     //        createCircle at x and y (recipe below) --#2
-    //        ------------- Recipe for createCircle --#3.0 (everything in this recipe)
-    //           Create a new circle with a radius of 11 using the next color on the color wheel 
-    //           Change the circle to be 60% opaque 
-    //           Move the circle so that it's center is at the current position of the mouse (x,y) 
-    //           Place the circle on the tortoise's window 
-    //        ------------- End of createCircle Recipe --#3.1
+    createCircle(x, y);
     //    Move the tortoise to the current position of the mouse (x,y) --#4
+    Tortoise.moveTo(x, y);
     //    ------------- End of addDot Recipe 
     //    Uncomment to write the text "Right click to clear the window" on the screen at position 100, 100 --#8
     //    new Text("Right click to clear the window").setTopLeft(100, 100).addTo(Tortoise.getBackgroundWindow());
+  }
+  public void createCircle(int x, int y)
+  {
+    //           Create a new circle with a radius of 11 using the next color on the color wheel 
+    Circle circle = new Circle(11, ColorWheel.getNextColor());
+    //           Change the circle to be 60% opaque 
+    circle.setMouseTransparent(60);
+    //           Move the circle so that it's center is at the current position of the mouse (x,y) 
+    circle.setCenter(x, y);
+    //           Place the circle on the tortoise's window 
+    circle.addTo(Tortoise.getBackgroundWindow());
   }
   @Override
   public void onRightMouseClick(int x, int y)
